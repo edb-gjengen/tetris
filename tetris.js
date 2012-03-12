291258ebb064fd28886f33cc586c4dc7ba60c887
Robert Kolner <robert.kolner@gmail.com>
2012-03-12 23:26:04 +0100

    More classes.

--- tetris.js
+++ tetris.js
@@ -1,19 +1,27 @@
-$(document).ready(function() {
-	var canvas = new TetrisCanvas("tetris-board");
-	var board = createArray(10,15);
-	
-	for (var y = 0; y < 15; y++) {
-		for (var x = 0; x < 10; x++) {
-			board[x][y] = 0;
+function TetrisGame(id) {
+
+	this.TetrisGame = function(id) {
+		var canvas = new TetrisCanvas(id);
+		var board = createArray(10,15);
+		
+		for (var y = 0; y < 15; y++) {
+			for (var x = 0; x < 10; x++) {
+				board[x][y] = 0;
+			}
 		}
-	}
-	
-	board[1][1] = 'I';
-	board[2][1] = 'J';
-	board[3][1] = 'L';
-	board[4][1] = 'O';
-	board[5][1] = 'S';
-	board[6][1] = 'T';
-	board[7][1] = 'Z';
-	canvas.drawBoard(board);
+		
+		board[1][1] = 'I';
+		board[2][1] = 'J';
+		board[3][1] = 'L';
+		board[4][1] = 'O';
+		board[5][1] = 'S';
+		board[6][1] = 'T';
+		board[7][1] = 'Z';
+		canvas.drawBoard(board);
+	};
+	this.TetrisGame(id);
+}
+
+$(document).ready(function() {
+	new TetrisGame("tetris-board");
 });
