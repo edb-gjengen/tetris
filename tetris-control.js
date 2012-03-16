24cdb2715f525fac4e6e335521b576b11a522889
Robert Kolner <robert.kolner@gmail.com>
2012-03-16 01:05:02 +0100

    Added arrows as controls.

--- tetris-control.js
+++ tetris-control.js
@@ -9,9 +9,25 @@ function initControls() {
 
 function onKeyDown(e) {
 	var ascii_space = 32;
-
+	var ascii_left = 37;
+	var ascii_right = 39;
+	var ascii_down = 40;
+	var ascii_up = 38;
+	
 	if (e.keyCode == ascii_space) {
 		game.hardDrop();
+	} else if (e.keyCode == ascii_left) {
+		game.moveCurrentBrickHorizontally(-1);
+	} else if (e.keyCode == ascii_right) {
+		game.moveCurrentBrickHorizontally(1);
+	} else if (e.keyCode == ascii_up) {
+		if (! wasRecentlyRotated) {
+			game.rotateCurrentBrick();
+			wasRecentlyRotated = true;
+		}
+		return;
+	} else if (e.keyCode == ascii_down) {
+		game.moveCurrentBrickDown();
 	}
 }
 
